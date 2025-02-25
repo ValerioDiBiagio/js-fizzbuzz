4b1a4593101e88a688b6956a0294db24584a836b
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-02-25 19:44:24 +0100

    feat: add third condition

--- js/main.js
+++ js/main.js
@@ -3,16 +3,19 @@
 let word;
 
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
 
-    if (i % 3 === 0) {
-        word="Fizz";
+    // Condizioni
+    
+    if (i % 3 === 0 && i % 5 === 0) {
+        word="FizzBuzz";
         console.log(word);
     } else if (i % 5 === 0) {
         word="Buzz";
         console.log(word);
-    }
-
+    } else if (i % 3 === 0) {
+        word="Fizz";
+        console.log(word);
+    } 
 
 }
 
